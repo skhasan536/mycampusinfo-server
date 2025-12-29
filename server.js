50b08b7be7d9fbcac0be0bc9f01b8eeb3e875543
skhasan536 <hasan.shaikh@wybrid.com>
2025-12-29 19:27:05 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send(getEntryPage());
+});
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+app.use((err, req, res, next) => {
+    console.error("Global Error:", err);
+    res.status(err.status || 500).json({
+        success: false,
+        message: err.message || "Internal Server Error",
+        error: err.toString()
+    });
+});
